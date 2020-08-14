37c0a4f804684aa55165d036d203ad6091361a7a
Moisés Gómez Medina <50937926+Nenva@users.noreply.github.com>
2020-08-14 12:22:59 -0700

    Initial commit

---
+++ app/src/main/java/mgm/u/myweatherapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package mgm.u.myweatherapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mgm.u.myweatherapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
